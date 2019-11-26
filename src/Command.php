47a27d50865317ce8f10af6b085b49ac1353d9d3
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2019-11-27 03:44:37 +0800

    Update Command.php

--- src/Command.php
+++ src/Command.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
 
 /**
- * Author: Chidume Nnamdi <kurtwanger40@gmail.com>
+ * Author: Ashraf Kamarudin <ashrafkamarudin@gmail.com>
  */
 class Command extends SymfonyCommand
 {
